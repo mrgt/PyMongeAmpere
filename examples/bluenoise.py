cce7728e9234e13bce937f741dafe132b5a510a2
Quentin Merigot <quentin@mrgt.fr>
2014-11-30 17:44:41 +0100

    Bluenoise example now accepts an image as input

--- examples/bluenoise.py
+++ examples/bluenoise.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('..');
-
+import os
 import MongeAmpere as ma
 import numpy as np
 import scipy as sp
@@ -8,7 +8,7 @@ import scipy.optimize as opt
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri
 
-n = 128;
+n = 256;
 [x,y] = np.meshgrid(np.linspace(-1,1,n),
                     np.linspace(-1,1,n));
 Nx = n*n;
@@ -16,31 +16,53 @@ x = np.reshape(x,(Nx));
 y = np.reshape(y,(Nx));
 X = np.vstack([x,y]).T;
 T = ma.delaunay_2(X,np.zeros(Nx));
-img = sp.misc.lena();
-#img = sp.misc.imread("calif.jpg", flatten=True);
+
+# load image, and resize/fix it for the stippling procedure
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+    cloudname = os.path.splitext(name)[0] + '.cloud';
+    img = sp.misc.imread(name, flatten=True);
+else:
+    cloudname = "lena.cloud";
+    img = sp.misc.lena();
 mu = sp.misc.imresize(img, (n,n));
 mu = sp.misc.imrotate(mu, 180);
 mu = np.reshape(mu,(Nx));
 mu = 255-np.asarray(mu, dtype=float);
 dens = ma.Density_2(X,mu,T);
-print(dens.mass());
 
-N = 20000;
-Y = 2*np.random.rand(N,2) - np.ones((N,2));
+N = 40000;
+Y = dens.random_sampling(N);
 nu = np.ones(N);
 nu = (dens.mass() / np.sum(nu)) * nu;
 
 
+# smoothen point cloud
 w = np.zeros(N);
-plt.figure(figsize=(10,10),facecolor="white")
-for i in xrange(1,10):
+for i in xrange(1,5):
     [Z,m] = ma.lloyd_2(dens, Y, w);
     Y = Z
+
+ww=10
+plt.figure(figsize=(ww,ww),facecolor="white")
+surf=(ww*100)*(ww*100);
+# N*s/surf = dens.mass()/2^2
+avg_gray = dens.mass()/(4*255);
+print avg_gray
+S = (surf/N)*avg_gray
+S = S/4
+print S
+
+for i in xrange(1,5):
     w = ma.optimal_transport_2(dens,Y,nu);
+    [Z,m] = ma.lloyd_2(dens, Y, w);
+    Y = Z
     plt.cla();
     plt.gca().set_aspect('equal')
-    plt.scatter(Y[:,0], Y[:,1], s=2);
+    plt.scatter(Y[:,0], Y[:,1], s=S);
+    plt.axis([-1,1,-1,1])
+    plt.axis('off')
     plt.pause(.01)
 plt.pause(10)
 
-
+# np.savetxt(cloudname, Y, fmt='%g')
